adcdf0282b22b1e42e455c7965269448d9dfa175
icarus999-2 <ismaelyloo14@gmail.com>
2025-02-17 11:44:06 -0500

    optimizar

---
+++ ocultar-mostrar.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.getElementById("ocultarsidebar");
+    const sidebar = document.getElementById("sidebarMenu");
+    const section = document.getElementById("content");
+
+    toggleButton.addEventListener("click", function () {
+sidebar.classList.toggle("hidden");
+
+// Alterna la clase que expande el contenido y el header
+if (sidebar.classList.contains("hidden")) {
+section.classList.add("full-width");
+} else {
+section.classList.remove("full-width");
+}
+});
+});
